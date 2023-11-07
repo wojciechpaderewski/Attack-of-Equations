4d3b798ee66ff15953cbe84ad734f2ad0c0d8bf6
Wojciech Paderewski <wojtpaderewski@gmail.com>
2023-11-07 21:55:22 +0100

    base loop

---
+++ game/src/Game.java
@@ -0,0 +1,80 @@
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable {
+    private boolean isRunning = false;
+    private Thread thread;
+
+    public Game() {
+        new Window(1024, 768, "Attack of Equations", this);
+        this.start();
+    }
+
+    public synchronized void start() {
+        if (isRunning) return;
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        if (!isRunning) return;
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        // write game loop limit to 60 fps
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks; // nanoseconds per tick
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns; // time passed divided by time per tick
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (isRunning) render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames); // print fps
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    // update game state
+    private void tick() {
+    }
+
+    // render game state
+    private void render() {
+        BufferStrategy bufferStrategy = this.getBufferStrategy();
+        if (bufferStrategy == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+        // draw here
+
+        graphics.dispose();
+        bufferStrategy.show();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Game();
+    }
+}
